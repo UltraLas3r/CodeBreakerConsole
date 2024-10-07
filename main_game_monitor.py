3c25bfff0e532e2337b48aab48d1368ded70c3a9
120864 <michael.schreiber@sncorp.com>
2024-10-07 09:10:06 -0700

    small building blocks for base game

---
+++ main_game_monitor.py
@@ -0,0 +1,12 @@
+
+
+def player_guess_counter(difficultyLevel: str, guessCount: int){
+    diff = difficultyLevel
+
+    if diff == "A":
+         else if diff == "B":
+         else if diff == "C":
+         else:
+    print("Player has chosen invalid difficulty")
+
+}
---
+++ player_choice_display.py
@@ -0,0 +1,3 @@
+# take player input, compare with game selected word. If character matches then replace
+# the _ with the matching letter.
+# reduce player guess count value by 1. if value == 0, then display failure screen.
